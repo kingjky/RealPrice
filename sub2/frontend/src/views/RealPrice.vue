4bae84c10e46a0a7390e21287671e12dce14236c
kingjky <kingjky@naver.com>
2020-04-08 09:34:47 +0900

    Modify realprice.vue
    Former-commit-id: c5c5e3835df523611e0fb7c5746a09e495fa5ad8

--- sub2/frontend/src/views/RealPrice.vue
+++ sub2/frontend/src/views/RealPrice.vue
@@ -3,11 +3,38 @@
     <v-card-text class="text-center">
       <p class="display-3 pa-2">🍜</p>
       <p class="display-2 pa-5">코딩도 식후경</p>
+      <v-slider
+          v-model="taste"
+          min="0"
+          max="100"
+          label="맛　"
+          thumb-label
+      ></v-slider>
+      <v-slider
+          v-model="distance"
+          min="0"
+          max="100"
+          label="거리"
+          thumb-label
+      ></v-slider>
+      <v-slider
+          v-model="price"
+          min="0"
+          max="100"
+          label="가격"
+          thumb-label
+      ></v-slider>
       <v-btn large color="blue lighten-1 white--text ma-5" rounded to="/search">검색하기</v-btn>
     </v-card-text>
   </v-container>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    taste: 50,
+    distance: 50,
+    price: 50,
+  }),
+};
 </script>
