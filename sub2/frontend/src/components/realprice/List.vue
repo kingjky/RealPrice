5a640a2b52f7744215fb7fdeae17f32523e52b33
kingjky <kingjky@naver.com>
2020-04-16 16:29:21 +0900

    Modify List.vue possible to scroll
    Former-commit-id: f684bd3cf683f20908a4eec0f88aae511b574e74

--- sub2/frontend/src/components/realprice/List.vue
+++ sub2/frontend/src/components/realprice/List.vue
@@ -1,5 +1,6 @@
 <template>
-  <v-container class="mt-5">
+  <v-container
+    id="scroll-target" class="mt-5 overflow-y-auto" >
     <!-- <h3>검색결과로 나온 결과값 - 검색결과에따라 다르게 보여줄 예정</h3>
     맛 : {{ RealPrice.taste }} <br>
     거리 : {{ RealPrice.distance }} <br>
@@ -35,39 +36,13 @@ export default {
   },
   data() {
     return {
-      stores: [
-        {
-          id: "id1",
-          name: "식당1",
-          categories: ["식당1의 카테고리1","식당1의 카테고리2"],
-          address: "식당1의 주소",
-          tel: "식당1의 전화번호",
-        },
-        {
-          id: "id2",
-          name: "식당2",
-          categories: ["식당2의 카테고리1","식당2의 카테고리2"],
-          address: "식당2의 주소",
-          tel: "식당2의 전화번호",
-        },
-        {
-          id: "id3",
-          name: "식당3",
-          categories: ["식당3의 카테고리1","식당3의 카테고리2"],
-          address: "식당3의 주소",
-          tel: "식당3의 전화번호",
-        }
-      ]
     }
   },
-  // computed: {
-  //   RealPrice: function() {
-  //     return this.$store.getters["data/RealPrice"];
-  //   }
-  // },
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+#scroll-target {
+  max-height: 90vh;
+}
 </style>
