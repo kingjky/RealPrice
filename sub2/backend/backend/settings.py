7936813711a5c2350d0139be5a45c7605aca7dde
James-Baek <ckdgus1428@gmail.com>
2020-04-17 10:07:10 +0900

    back fix
    Former-commit-id: ed2739568bcd4e664964d196b61d0b9f14bcb38f

--- sub2/backend/backend/settings.py
+++ sub2/backend/backend/settings.py
@@ -65,7 +65,6 @@ REST_FRAMEWORK = {
 REST_USE_JWT = True
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware"
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -73,6 +72,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware"
 ]
 
 if DEBUG:
