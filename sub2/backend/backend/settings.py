04a25e4f4d8a96e6e74575478a1f9d07c9412870
James-Baek <ckdgus1428@gmail.com>
2020-04-17 10:00:54 +0900

    cors fix
    Former-commit-id: dff92fac820be8731958f1a5973a3385e80b4f6b

--- sub2/backend/backend/settings.py
+++ sub2/backend/backend/settings.py
@@ -65,6 +65,7 @@ REST_FRAMEWORK = {
 REST_USE_JWT = True
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware"
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -72,7 +73,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'corsheaders.middleware.CorsMiddleware'
 ]
 
 if DEBUG:
