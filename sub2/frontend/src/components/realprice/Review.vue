46211ef75f6e68c75997dc49d56d08a39e2956a3
juyeon <wjg365@naver.com>
2020-04-23 15:24:34 +0900

    card css 수정
    Former-commit-id: 7adaf2774572553864e2987489a85f1b22a13776

--- sub2/frontend/src/components/realprice/Review.vue
+++ sub2/frontend/src/components/realprice/Review.vue
@@ -1,29 +1,21 @@
 <template>
   <v-row dense>
-    <v-card outlined>
-      <v-card max-width="400">
-        <v-card-text>
-          <v-avatar size="30">
-            <v-img class="card-img" src="https://cdn.vuetifyjs.com/images/john.jpg" />
-          </v-avatar>
-          {{ review.content.slice(0, 50) }} ...
-          <span class="red lighten-4 white--text">더보기</span>
-          <p>작성일 : {{ review.reg_time }}</p>
-          <p>평점 : {{ review.score }}</p>
-        </v-card-text>
+    <v-col cols="10">
+      <v-card outlined>
+        <v-card>
+          <v-card-text>
+            <v-avatar size="30">
+              <v-img class="card-img" src="https://cdn.vuetifyjs.com/images/john.jpg" />
+            </v-avatar>
+            {{ review.content.slice(0, 50) }} ...
+            <span class="red lighten-4 white--text">더보기</span>
+            <br />
+            작성일 : {{ review.reg_time }}
+            <p>평점 : {{ review.score }}</p>
+          </v-card-text>
+        </v-card>
       </v-card>
-      <v-card max-width="200">
-        <v-card-text>
-          <div>Word of the Day</div>
-          <p class="display-1 text--primary">be•nev•o•lent</p>
-          <p>adjective</p>
-          <div class="text--primary">
-            well meaning and kindly.
-            <br />"a benevolent smile"
-          </div>
-        </v-card-text>
-      </v-card>
-    </v-card>
+    </v-col>
   </v-row>
 </template>
 
